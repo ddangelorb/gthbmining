bb81e848f2b431dc918688a649d6926f58192c78
Daniel D'Angelo Resende Barros <ddangelorb@outlook.com>
2020-06-08 07:05:23 -0300

    .

---
+++ rc/loaddata/config.py
@@ -0,0 +1,8 @@
+github_user = ""
+github_pwd = ""
+github_note = "gthbmining"
+github_note_url = "https://github.com/ddangelorb/gthbmining"
+github_scopes = ['user', 'repo']
+github_token = ""
+repo_user = ""
+repo_name = ""
